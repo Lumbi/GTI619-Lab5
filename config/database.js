890ae93f39b57d76048d3129b5019bad296dcea8
goldylock <guslep@gmail.com>
2014-11-13 16:36:09 -0500

    connection with modulus

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'admin:banane@locahost:27017/local' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://alice:root@proximus.modulusmongo.net:27017/Pejox3ox' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
